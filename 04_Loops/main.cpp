7eed4cb07472a195b3631eac548ab50ec9837ec8
Alexander Schelchere <ascellie@gmail.com>
2020-10-14 12:07:52 -0400

    ch04: loops

---
+++ 04_Loops/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Typing
+    cout << "Hello, friend!\n";
+    cout << "Hello, friend!\n";
+    cout << "Hello, friend!\n";
+    cout << "Hello, friend!\n";
+    cout << "Hello, friend!\n";
+
+    // While Loop
+    int x = 0;
+    while(x < 5) {
+        cout << "How are you?\n";
+        x++;
+    }
+
+    // For Loop
+    for(int i = 5; i > 0; i--) {
+        cout << "TTYL\n";
+    }
+
+
+    return 0;
+}
